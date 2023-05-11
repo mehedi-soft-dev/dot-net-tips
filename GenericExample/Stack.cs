5b5bb7227585870cab39b46039d392ca83b48d7f
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-05-11 23:20:21 +0600

    Add Generic Example

---
+++ GenericExample/Stack.cs
@@ -0,0 +1,33 @@
+﻿namespace GenericExample;
+
+public class Stack<T>
+{
+    private List<T> elements;
+
+    public Stack()
+    {
+        elements = new List<T>();
+    }
+
+    public void Push(T item)
+    {
+        elements.Add(item);
+    }
+
+    public T Pop()
+    {
+        if (elements.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        T item = elements[elements.Count - 1];
+        elements.RemoveAt(elements.Count - 1);
+        return item;
+    }
+
+    public int Count
+    {
+        get { return elements.Count; }
+    }
+}
