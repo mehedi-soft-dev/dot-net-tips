d74890d44bdf047c18610f2ac269782451e9391d
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-05-12 23:18:30 +0600

    Add Delegate Example

---
+++ DelegateExample/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public delegate void PrintDelegate(object message);
+
+    public static void Calculate(int a, int b, PrintDelegate printer)
+    {
+        int result = a + b;
+        printer(result);
+    }
+
+    public static void Main(string[] args)
+    {
+        //Pass Print Method
+        Calculate(10, 5, Print);
+        
+        //Pass Print2 Method
+        Calculate(2, 3, Print2);
+    }
+
+    public static void Print(object message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void Print2(object message)
+    {
+        Console.WriteLine("---" + message + "---");
+    }
+}
